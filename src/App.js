c37e51138ecdb2e4349ec909da3c081b0da63169
Bryan <bryanbuitrago86@gmail.com>
2020-05-12 16:13:01 -0400

    Finish sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -33,8 +33,6 @@ class App extends React.Component {
               }
             }
           );
-
-          console.log(this.state)
         })
       }
       // if User logs out set session to null
